57167b646e12af52937042be142168c84588cb2c
complover116 <complover116@gmail.com>
2016-05-15 16:03:35 +0300

    Inital commit v2

---
+++ html/src/com/starbangers/sasus/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.starbangers.sasus.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.starbangers.sasus.SASUS;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new SASUS();
+        }
+}
