37e5712338d199c432a600f5776f9a33b5876eea
complover116 <complover116@gmail.com>
2016-05-23 21:46:50 +0300

    Player can now be controlled from keyboard

--- core/src/com/starbangers/sasus/Player.java
+++ core/src/com/starbangers/sasus/Player.java
@@ -1,11 +1,17 @@
 package com.starbangers.sasus;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Player {
+	public static final float BASE_SPEED = 250;
+	
+	
 	private Sprite sprite;
 	
-	float x = 300;
+	float x = 0;
+	float goalX = 300;
 	float y = 32;
 	float rot = 3;
 	float goalRot = 0;
@@ -16,7 +22,17 @@ public class Player {
 	}
 	
 	public void update(float deltaT) {
-		rot += (goalRot - rot)*deltaT;
+		rot += (goalRot - rot)*deltaT*8;
+		goalRot = -(goalX-x)/200;
+		
+		x += (goalX - x)*deltaT*3;
+		
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			goalX += BASE_SPEED*deltaT;
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			goalX -= BASE_SPEED*deltaT;
+		}
 	}
 	
 	public void draw() {
