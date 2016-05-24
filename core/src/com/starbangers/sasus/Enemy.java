358e08f3bafbef8a26fdbf28b376b2975765d483
complover116 <complover116@gmail.com>
2016-05-24 20:48:06 +0300

    Added enemy graphic

---
+++ core/src/com/starbangers/sasus/Enemy.java
@@ -0,0 +1,23 @@
+package com.starbangers.sasus;
+
+public class Enemy extends Entity {
+
+	@Override
+	public boolean tick(float deltaT) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawShapes() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
