aeaa72e84f5f9a4055fa939cbb22d2ec8056550c
QwertygidQ <Qwertygid@gmail.com>
2016-06-20 23:13:56 +0300

    Git gud

--- core/src/com/starbangers/sasus/EnemyGroup.java
+++ core/src/com/starbangers/sasus/EnemyGroup.java
@@ -1,22 +1,26 @@
 package com.starbangers.sasus;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class EnemyGroup
 {
-	float startTime, endTime;
-	int count;
+	private float startTime;
+	private int enemiesCount, curvesCount;
+	private Vector2 startingPoint;
+	
+	public EnemyGroup(float _startTime, int _enemiesCount, int _curvesCount)
+	{
+		startTime = _startTime;
+		enemiesCount = _enemiesCount;
+		curvesCount = _curvesCount;
+		
+		startingPoint = new Vector2();
+		startingPoint.x = Math.random() > 0.5 ? -100 : 900;
+		startingPoint.y = (float) (Math.random() * 800 - 100);
+	}
 	
 	public void register() {
 		GameMaster.groups.add(this);
 	}
 	
-	public EnemyGroup setStartEndTime(float startTime, float endTime) {
-		this.startTime = startTime;
-		this.endTime = endTime;
-		return this;
-	}
-	
-	public EnemyGroup setCount(int count) {
-		this.count = count;
-		return this;
-	}
 }
