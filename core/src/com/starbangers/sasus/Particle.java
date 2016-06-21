a3d822ffd35f7565e87b270b1e538f028a6e5be9
complover116 <complover116@gmail.com>
2016-06-21 15:02:22 +0300

    Enemies now BOOM

--- core/src/com/starbangers/sasus/Particle.java
+++ core/src/com/starbangers/sasus/Particle.java
@@ -8,10 +8,14 @@ public class Particle {
 	Shape shape;
 	
 	float r, g, b;
-	
+	float startR, startG, startB, endR, endG, endB;
 	float x, y, velX, velY, size, decay;
 	
+	float lifeTime;
+	
 	boolean glow;
+	boolean transColor;
+	float gravity;
 	
 	public Particle(float _x, float _y, float _velX, float _velY, float _size, float _decay, float _r, float _g, float _b, Shape _shape, boolean _glow) {
 		this.x = _x;
@@ -29,6 +33,10 @@ public class Particle {
 	public Particle(Shape shape) {
 		this.shape = shape;
 	}
+	public Particle setGravity(float gravity) {
+		this.gravity = gravity;
+		return this;
+	}
 	public Particle setPos(float x, float y) {
 		this.x = x;
 		this.y = y;
@@ -43,6 +51,20 @@ public class Particle {
 		this.r = r;
 		this.g = g;
 		this.b = b;
+		this.transColor = false;
+		return this; //Method chaining
+	}
+	public Particle setFadingColors(float r, float g, float b, float r2, float g2, float b2) {
+		this.startR = r;
+		this.startG = g;
+		this.startB = b;
+		this.r = startR;
+		this.g = startG;
+		this.b = startB;
+		this.endR = r2;
+		this.endG = g2;
+		this.endB = b2;
+		this.transColor = true;
 		return this; //Method chaining
 	}
 	public Particle setGlowing(boolean glow) {
@@ -52,6 +74,7 @@ public class Particle {
 	public Particle setSizeAndDecay(float size, float decay) {
 		this.size = size;
 		this.decay = decay;
+		this.lifeTime = this.size/this.decay;
 		return this;
 	}
 	public void spawn() {
@@ -63,9 +86,15 @@ public class Particle {
 	}
 	
 	public boolean tick(float deltaT) {
+		this.velY -= this.gravity*deltaT;
 		this.x += velX*deltaT;
 		this.y += velY*deltaT;
 		this.size -= this.decay*deltaT;
+		if(true) {
+			this.r += (this.endR - this.startR)/this.lifeTime*deltaT;
+			this.g += (this.endG - this.startG)/this.lifeTime*deltaT;
+			this.b += (this.endB - this.startB)/this.lifeTime*deltaT;
+		}
 		if(size < 0)return false;
 		return true;
 	}
